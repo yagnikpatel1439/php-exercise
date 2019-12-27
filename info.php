b97993f64208fe8c1c6db9faaaa54d0cc2636040
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2019-12-27 17:11:04 -0500

    display user info (funny and succintly)

---
+++ info.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Display Info</title>
+</head>
+<body>
+    Hi <?php echo $_POST['salutation'], $_POST['first_name'], $_POST['last_name']  ?>.
+
+    To welcome onboard you've given this info. You're <?php echo $_POST['gender'] ?> and want to eat <?php echo $_POST['diet_pref'] ?>. Also you're accompany with <?php echo $_POST['passenger_no'] ?> more people.
+    
+    <?php 
+        if ($_POST['nationality'] !== 'true') {
+            echo "Your nationality is other than Indian.";
+        } else {
+            echo "Your nationality is Indian!";
+        }
+    ?>
+</body>
+</html>
