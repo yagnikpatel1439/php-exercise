87acfab32ec45bb49ccf8189eaa1e859432a82b9
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2019-12-28 18:31:56 -0500

    indentation fixed

--- index.php
+++ index.php
@@ -43,57 +43,57 @@ if (isset($_POST['submit'])) {
 </head>
 <body>
     <div class="info">
-    <form role= "form" action="" method="post" id="info">
-        <div class="form-group">
-            <label for="first_name">Firstname: </label>
-            <input type="text" name="first_name">
-        </div>
-        <br>
-        <div>     
-            <label for="last_name">Lastname: </label>
-            <input type="text" name="last_name">  
-        </div>
-        <br>
-        <div>
-            <label for="salutation"> Select yout salutation: </label>
-            <select name="salutation" id="salutation">
-                <option value="mr">Mr</option>
-                <option value="mrs">Mrs</option>
-                <option value="none">None</option>
-            </select>
-        </div>
-        <br>
-        <div>
-            <label for="passengers" value="0">How many people are flying with you?</label>
-            <input type="number" name="passenger_no" min="0" max="10">
-        </div>
-        <br>
-        <div>
-            <label for="nationality">Are you an Indian?</label>
-            <input type="radio" name="nationality" value="true">True 
-            <input type="radio" name="nationality" value="false">False 
-        </div>
-        <br>
-        <div>
-            <label for="gender">Define your gender:</label>
-            <input type="radio" name="gender" value="male">Male
-            <input type="radio" name="gender" value="female">Female
-        </div>
-        <br>
-        <div>
-            <label for="diet">Your diet preference: </label>
-            <select name="diet_pref">
-                <option value="vegan">Vegan</option>
-                <option value="Lacto Vegetarian(includes dairy)">Lacto Vegetarian(includes dairy)</option>
-                <option value="Ovo Vegetarian(includes eggs)">Ovo Vegetarian(includes eggs)</option>
-                <option value="Pescatarian(includes fish)">Pescatarian(includes fish)</option>
-            </select>
-        </div>
-        <br>
-        <input type="submit" value="submit" name="submit">
+        <form role= "form" action="" method="post" id="info">
+            <div class="form-group">
+                <label for="first_name">Firstname: </label>
+                <input type="text" name="first_name">
+            </div>
+            <br>
+            <div>     
+                <label for="last_name">Lastname: </label>
+                <input type="text" name="last_name">  
+            </div>
+            <br>
+            <div>
+                <label for="salutation"> Select yout salutation: </label>
+                <select name="salutation" id="salutation">
+                    <option value="mr">Mr</option>
+                    <option value="mrs">Mrs</option>
+                    <option value="none">None</option>
+                </select>
+            </div>
+            <br>
+            <div>
+                <label for="passengers" value="0">How many people are flying with you?</label>
+                <input type="number" name="passenger_no" min="0" max="10">
+            </div>
+            <br>
+            <div>
+                <label for="nationality">Are you an Indian?</label>
+                <input type="radio" name="nationality" value="true">True 
+                <input type="radio" name="nationality" value="false">False 
+            </div>
+            <br>
+            <div>
+                <label for="gender">Define your gender:</label>
+                <input type="radio" name="gender" value="male">Male
+                <input type="radio" name="gender" value="female">Female
+            </div>
+            <br>
+            <div>
+                <label for="diet">Your diet preference: </label>
+                <select name="diet_pref">
+                    <option value="vegan">Vegan</option>
+                    <option value="Lacto Vegetarian(includes dairy)">Lacto Vegetarian(includes dairy)</option>
+                    <option value="Ovo Vegetarian(includes eggs)">Ovo Vegetarian(includes eggs)</option>
+                    <option value="Pescatarian(includes fish)">Pescatarian(includes fish)</option>
+                </select>
+            </div>
+            <br>
+            <input type="submit" value="submit" name="submit">
+        </form>
     </div>
-    </form>
     <br>
-    <a href="data_dump.php">Data</a>
+    <button style="background-color: #4CAF50;"><a href="data_dump.php">Get Data</a></button>
 </body>
 </html>
